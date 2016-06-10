c8fc512390962a3c1615ee7c9c522dc5172ec57e
Jakub Buriánek <burianekj@cngroup.dk>
2016-06-10 15:16:09 +0200

    Install ICU plugin for collation

--- recipes/search_plugins.rb
+++ recipes/search_plugins.rb
@@ -2,3 +2,7 @@
 simple_chef_tools_safebash "elastic_plugin kopf" do
   code "/usr/share/elasticsearch/bin/plugin install lmenezes/elasticsearch-kopf/2.0"
 end
+
+simple_chef_tools_safebash "elastic_plugin kopf" do
+  code "/usr/share/elasticsearch/bin/plugin install analysis-icu"
+end
