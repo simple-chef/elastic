237dfe76c43be47bfee9bcc252ba73e25eb729e3
mojzis <mojzisek@gmail.com>
2016-06-13 17:51:55 +0200

    rename safebash call

--- recipes/search_plugins.rb
+++ recipes/search_plugins.rb
@@ -3,6 +3,6 @@ simple_chef_tools_safebash "elastic_plugin kopf" do
   code "/usr/share/elasticsearch/bin/plugin install lmenezes/elasticsearch-kopf/2.0"
 end
 
-simple_chef_tools_safebash "elastic_plugin kopf" do
+simple_chef_tools_safebash "elastic_plugin icu" do
   code "/usr/share/elasticsearch/bin/plugin install analysis-icu"
 end
